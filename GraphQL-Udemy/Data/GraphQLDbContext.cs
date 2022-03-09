ebf1510221ec98f497117748e8601361f31b4f84
Ido Tanne <idovader@gmail.com>
2022-03-09 10:24:04 -0500

    Added Data folder

---
+++ GraphQL-Udemy/Data/GraphQLDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GraphQL_Udemy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL_Udemy.Data
+{
+    public class GraphQLDbContext : DbContext
+    {
+        public GraphQLDbContext(DbContextOptions<GraphQLDbContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(c => c.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
