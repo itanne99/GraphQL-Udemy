6dab73555721abc5eaaf77b4dcc381c5506847dc
Ido Tanne <idovader@gmail.com>
2022-03-24 22:37:48 -0400

    Added Input Type

---
+++ GraphQL-Udemy/Type/MenuInputType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+
+namespace GraphQL_Udemy.Type
+{
+    public class MenuInputType : InputObjectGraphType
+    {
+        public MenuInputType()
+        {
+            Field<IntGraphType>("id");
+            Field<StringGraphType>("name");
+            Field<StringGraphType>("imageURL");
+        }
+    }
+}
