de4d8d3a095e4fb1e632943993c67296699e9b2c
Ido Tanne <idovader@gmail.com>
2022-03-24 22:13:32 -0400

    Added Queries

---
+++ GraphQL-Udemy/Query/MenuQuery.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using GraphQL_Udemy.Interfaces;
+using GraphQL_Udemy.Type;
+
+namespace GraphQL_Udemy.Query
+{
+    public class MenuQuery : ObjectGraphType
+    {
+        public MenuQuery(IMenu menuService)
+        {
+            Field<ListGraphType<MenuType>>("menus",
+                resolve: context => { return menuService.GetMenus(); });
+        }
+    }
+}
---
+++ GraphQL-Udemy/Query/RootQuery.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+
+namespace GraphQL_Udemy.Query
+{
+    public class RootQuery : ObjectGraphType
+    {
+        public RootQuery()
+        {
+            Field<MenuQuery>("menuQuery", resolve: context => new { });
+            Field<SubMenuQuery>("subMenuQuery", resolve: context => new { });
+            Field<ReservationQuery>("reservationQuery", resolve: context => new { });
+        }
+        
+    }
+}
