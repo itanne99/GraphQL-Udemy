0adcbdde1afb6ef7527e750bf3841d2ca7eeb127
Ido Tanne <idovader@gmail.com>
2022-03-24 21:50:29 -0400

    Added Models to DbContext

--- GraphQL-Udemy/Data/GraphQLDbContext.cs
+++ GraphQL-Udemy/Data/GraphQLDbContext.cs
@@ -11,33 +11,24 @@ namespace GraphQL_Udemy.Data
             
         }
 
-        public DbSet<Product> Products { get; set; } // TODO: Remove
+        public DbSet<Menu> Menus { get; set; }
+        
+        public DbSet<SubMenu> SubMenus { get; set; }
 
-        public DbSet<Song> Songs { get; set; }
-        public DbSet<Artist> Artists { get; set; }
-        public DbSet<Album> Albums { get; set; }
-        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Product>() // TODO: Remove
+            modelBuilder.Entity<Menu>() 
                 .Property(f => f.Id)
                 .ValueGeneratedOnAdd();
-
-            modelBuilder.Entity<Song>()
-                .Property(f => f.ID)
+            
+            modelBuilder.Entity<SubMenu>() 
+                .Property(f => f.Id)
                 .ValueGeneratedOnAdd();
-
-            modelBuilder.Entity<Artist>()
-                .Property(f => f.Artist_ID)
-                .ValueGeneratedOnAdd();
-
-            modelBuilder.Entity<Album>()
-                .Property(f => f.Album_ID)
-                .ValueGeneratedOnAdd();
-
-            modelBuilder.Entity<Genre>()
-                .Property(f => f.Genre_ID)
+            
+            modelBuilder.Entity<Reservation>() 
+                .Property(f => f.Id)
                 .ValueGeneratedOnAdd();
         }
     }
