86cbb23e17c9a0cb37855d6ccfe42dc9cafa0fb5
Ido Tanne <idovader@gmail.com>
2022-03-25 00:50:51 -0400

    Added Queries

---
+++ GraphQL-Udemy/Query/GenreQuery.cs
@@ -0,0 +1,27 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using GraphQL_Udemy.Interfaces;
+using GraphQL_Udemy.Type;
+
+namespace GraphQL_Udemy.Query
+{
+    public class GenreQuery : ObjectGraphType
+    {
+        public GenreQuery(IGenre genreService)
+        {
+            //Get all
+            Field<ListGraphType<GenreType>>("genres",
+                resolve: context => { return genreService.GetGenres(); });
+
+            //Get by ID
+            Field<GenreType>("genreById",
+                arguments: new QueryArguments(new QueryArgument<IntGraphType> {Name = "id"}),
+                resolve: context => { return genreService.GetGenre(context.GetArgument<int>("id")); });
+            
+            //Get by Name
+            Field<GenreType>("genreByName",
+                arguments: new QueryArguments(new QueryArgument<StringGraphType> {Name = "name"}),
+                resolve: context => { return genreService.GetGenre(context.GetArgument<string>("name")); });
+        }
+    }
+}
