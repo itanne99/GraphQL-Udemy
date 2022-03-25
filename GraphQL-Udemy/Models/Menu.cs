653f6bfcf8136d286ca814414b522fe19c618e3f
Ido Tanne <idovader@gmail.com>
2022-03-24 21:43:39 -0400

    Added Menu/SubMenu Models

---
+++ GraphQL-Udemy/Models/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace GraphQL_Udemy.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public ICollection<SubMenu> SubMenus { get; set; } // Creates a one-to-many relationship
+        
+    }
+}
---
+++ GraphQL-Udemy/Models/SubMenu.cs
@@ -0,0 +1,16 @@
+﻿namespace GraphQL_Udemy.Models
+{
+    public class SubMenu
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public int MenuId { get; set; }
+        
+    }
+}
