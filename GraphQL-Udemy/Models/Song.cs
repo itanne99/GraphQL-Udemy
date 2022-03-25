0ba2d7d925efe2a8f688185ac0e48657d3e43945
Ido Tanne <idovader@gmail.com>
2022-03-25 02:25:22 -0400

    Added artist/album/genre view to songQuery

--- GraphQL-Udemy/Models/Song.cs
+++ GraphQL-Udemy/Models/Song.cs
@@ -7,9 +7,12 @@
         public string Name { get; set; }
 
         public Artist Artist { get; set; }
+        public int ArtistId { get; set; }
         
         public Album Album { get; set; }
+        public int AlbumId { get; set; }
 
         public Genre Genre { get; set; }
+        public int GenreId { get; set; }
     }
 }
