a0264356b5b1a716bc394c37bc10c2f195d81755
Ido Tanne <idovader@gmail.com>
2022-03-24 21:45:13 -0400

    Added Reservation model

---
+++ GraphQL-Udemy/Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿namespace GraphQL_Udemy.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public int TotalPeople { get; set; }
+
+        public string Email { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+    }
+}
