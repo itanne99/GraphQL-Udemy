1edd00e5d1e50ca533e5bd7b87f2ec1d287bbfc2
Ido Tanne <idovader@gmail.com>
2022-03-24 22:33:01 -0400

    Added Schema

---
+++ GraphQL-Udemy/Schema/RootSchema.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GraphQL.Utilities;
+using GraphQL_Udemy.Query;
+
+namespace GraphQL_Udemy.Schema
+{
+    public class RootSchema : GraphQL.Types.Schema
+    {
+        public RootSchema(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            Query = serviceProvider.GetRequiredService<RootQuery>();
+        }
+    }
+}
