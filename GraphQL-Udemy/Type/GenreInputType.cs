6e8a815e21cab03a407332336d464230ee9d7e0b
Ido Tanne <idovader@gmail.com>
2022-03-25 00:53:54 -0400

    Added InputType

---
+++ GraphQL-Udemy/Type/GenreInputType.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Types;
+
+namespace GraphQL_Udemy.Type
+{
+    public class GenreInputType : InputObjectGraphType
+    {
+        public GenreInputType()
+        {
+            Field<IntGraphType>("id");
+            Field<StringGraphType>("name");
+        }
+    }
+}
